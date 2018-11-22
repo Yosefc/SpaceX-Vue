d0f9c753e282b3f70836ef81d8f09c99d4506fc8
Yosefc <yosef.conn@gmail.com>
2018-11-22 10:13:53 +0200

    Fixed cors bug

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const grapqlHTTP = require("express-graphql");
 const PORT = process.env.PORT || 4000;
+const cors = require("cors");
 const schema = require("./schema");
 const app = express();
 
+app.use(cors());
 app.use(
   "/graphql",
   grapqlHTTP({
