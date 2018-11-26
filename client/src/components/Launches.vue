334d452a851ea9d326813c82b5b593cd88940959
Yosefc <yosef.conn@gmail.com>
2018-11-26 12:43:19 +0200

    Updated store to manage the API data

--- client/src/components/Launches.vue
+++ client/src/components/Launches.vue
@@ -1,55 +1,36 @@
 <template>
-  <div id="loader" v-if="!launches.data">
-    <h5>Loading.....</h5>
+  <div id="loader" v-if="!getAllLaunches.data">
+    <h5>Loading...</h5>
   </div>
   <div v-else class="container">
     <div class="row">
         <Launch 
         class="card border-primary col-lg-12" 
         :key="launch.flight_number" 
-        v-for="launch in launches.data.Launches"
+        v-for="launch in getAllLaunches.data.Launches"
         :launch="launch"></Launch>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 import Launch from "./Launch.vue";
 
 export default {
   components: {
     Launch
   },
-  data() {
-    return {
-      launches: ""
-    };
+  computed: {
+    getAllLaunches() {
+      return this.$store.state.launches;
+    }
   },
   mounted() {
-    try {
-      axios
-        .post("http://localhost:4000/graphql", {
-          query: `{
-            Launches {
-              flight_number
-              mission_name
-              launch_year
-              launch_date_local
-              launch_success
-              details
-            }
-          }`
-        })
-        .then(res => (this.launches = res.data));
-    } catch (error) {
-      this.launches = error;
-    }
+    this.$store.dispatch("fetchLaunches");
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #loader {
   padding-top: 50px;
--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,38 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    launches: ""
   },
   mutations: {
-
+    updateLaunches(state, data) {
+      state.launches = data;
+    }
   },
   actions: {
-
+    fetchLaunches({ commit }) {
+      try {
+        axios
+          .post("http://localhost:4000/graphql", {
+            query: `{
+              Launches {
+                flight_number
+                mission_name
+                launch_year
+                launch_date_local
+                launch_success
+                details
+              }
+            }`
+          })
+          .then(res => commit("updateLaunches", res.data));
+      } catch (error) {
+        commit("updateLaunches", error);
+      }
+    }
   }
-})
+});
